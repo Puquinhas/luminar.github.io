963009cc863c528efacd45ac0237bf655bcddba5
luminar <33518462+projetoluminar@users.noreply.github.com>
2018-11-27 22:07:20 -0200

    Upload
    blá

--- js/app.js
+++ js/app.js
@@ -12,3 +12,21 @@ $('.mobile__btn').click(function(){
     $(this).toggleClass('active');
     $('.mobile__menu').toggleClass('active');
 });
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 40) {
+            $('#back__top').fadeIn();
+        } else {
+            $('#back__top').fadeOut();
+        }
+    });
+    $('#back__top').click(function(){
+        $('html, body').animate({scrollTop: 0}, 800);
+    });
+});
+
+$('.experimento').click(function() {
+    $(this).next().slideToggle(400);
+    $(this).toggleClass('active');
+});
